4ece2d12008886c4b01abde5cddcc66e9bc040f5
Rsy0506 <fbtkddus123@naver.com>
2020-01-04 11:02:54 +0900

    DB v.0.0

---
+++ Desktop/CommonGood/commongood/routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require("mongoose");
+var User = require('C:\Users\fbtkd\Desktop\CommonGood\commongood\schemas/user');
+
+/* GET users listing. */
+router.post('/Signup', function(req, res, next) {
+  User.find({id:req.body.email})
+  .exec()
+    .then(user => {
+        if (user.length >= 1) {
+            res.send('<script type="text/javascript">alert("이미 존재하는 회원입니다."); window.location="/Signup"; </script>');
+        } 
+        else {
+          const user = new User({
+            username: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          });
+          user.save()
+            .then((result)=>{
+            console.log(result);
+            res.status(201).json(result);
+          })
+            .catch((err)=>{
+              console.error(err);
+              next(err);
+            });
+        }
+  });
+});
+
+router.get('/Login', function(req, res, next){
+
+});
+router.get('/mypage', function(req, res, next) {
+
+});
+
+module.exports = router;
